f01ffeff05822fae5b959c9f0066b373bd8bf8da
y.kohama <yk050696@gmail.com>
2020-10-01 17:33:09 +0900

    test

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,6 +2,4 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
  
 module.exports = function(app) {
 　app.use(createProxyMiddleware('/api', { target: 'http://localhost:4000' }));
-　app.use(createProxyMiddleware('/api/*/*', { target: 'http://localhost:4000' }));
-　app.use(createProxyMiddleware('/api/*', { target: 'http://localhost:4000' }));
 }
